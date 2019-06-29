5dc82dab95fab982030db21b911fcea92fad99f2
Bill Gauvey <bgauvey@gmail.com>
2019-06-29 07:59:05 -0400

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ManagementComponent } from './management/management.component';
+import { InteractiveAnalyticsComponent } from './interactive-analytics/interactive-analytics.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'management', component: ManagementComponent },
+  { path: 'interactive-analytics', component: InteractiveAnalyticsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InteractiveAnalyticsComponent } from './interactive-analytics/interactive-analytics.component';
+import { ManagementComponent } from './management/management.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    InteractiveAnalyticsComponent,
+    ManagementComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
